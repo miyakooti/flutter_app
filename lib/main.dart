b85a7ccadebc3abdc6fb92dce74a1f3fbb8cfb49
arai <niimaru09@gmail.com>
2023-03-09 20:57:44 +0900

    あそんでる

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,160 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  final title = "flutter sample";
-  final message = "sample message";
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "flutter demo",
-      home: MyHomePage(
-        title: this.title,
-        message: this.message
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
   final String title;
-  final String message;
-  const MyHomePage({
-    Key? key,
-    required this.title,
-    required this.message
-  }): super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title,
+        selectionColor: Colors.black,),
       ),
-      body: Text(
-        widget.message,
-        style: TextStyle(fontSize: 32.0),
+      body: Center(
+        child: Container(
+          color: Colors.blueGrey,
+          height: 200,
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+
+              children: <Widget>[
+                const Text('You have pushed the button this many times:',),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// text ---------------------------------------------------------------------------
+
+
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//   final title = "flutter sample";
+//   final message = "sample message";
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "flutter demo",
+//       home: MyHomePage(
+//         title: this.title,
+//         message: this.message
+//       ),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   final String title;
+//   final String message;
+//   const MyHomePage({
+//     Key? key,
+//     required this.title,
+//     required this.message
+//   }): super(key: key);
+//
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Text(
+//         widget.message,
+//         style: TextStyle(fontSize: 32.0),
+//       ),
+//     );
+//   }
+// }
+
+
+// /text ---------------------------------------------------------------------------
 
 
 // class MyApp extends StatelessWidget {

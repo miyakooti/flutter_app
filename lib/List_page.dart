f6087ab20fc294ad42d79208801e7986d6723fbb
arai <niimaru09@gmail.com>
2023-03-10 11:05:44 +0900

    listviewで遊んでる

--- lib/List_page.dart
+++ lib/List_page.dart
@@ -1,31 +1,28 @@
 import "package:flutter/material.dart";
 
 class ListPage extends StatelessWidget {
+  final List<String> entries = <String>['A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("ListPage"),
         ),
-        body: ListView(
-          padding: const EdgeInsets.all(8),
-          children: <Widget>[
-            Container(
-              height: 50,
-              color: Colors.amber[600],
-              child: const Center(child: Text('Entry A')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.amber[500],
-              child: const Center(child: Text('Entry B')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.amber[100],
-              child: const Center(child: Text('Entry C')),
-            ),
-          ],
-        ));
+        body: ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                color: Colors.amber[colorCodes[index]],
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Center(
+                      child: Text('Entry ${entries[index]}',
+                          style: TextStyle(fontSize: 50))),
+                ),
+              );
+            }));
   }
 }
